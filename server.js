52dec773d0c5da4a7386f4793f2746aeeafd0281
tamimamo <tamimamo@gmail.com>
2016-06-20 17:14:49 +0300

    20/06/2016 0.0.1

--- server.js
+++ server.js
@@ -3,26 +3,42 @@ var express = require('express'); // for routing
 var app = express(); //init the server
 var path = require('path');
 var mongoose = require('mongoose');
-var catalogSchema = require('./DataBase/catalog');
-var Catalog = mongoose.model('Catalog', catalogSchema);
+//var catalogSchema = require('./DataBase/catalog');
+//var Catalog = mongoose.model('Catalog', catalogSchema);
+
+
+config={
+    mongoURL:'mongodb://paleta:123@ds038739.mlab.com:38739/paleta_db'
+};
+
+var options=
+{
+    server:
+    {
+        auto_reconnect: true,
+    }
+};
+
+var db = mongoose.createConnection(config.mongoURL, options);
+
+//db = mongoose.connection;// a global connection variable
+// Event handlers for Mongoose
+db.on('error', function (err) {
+    console.log('Mongoose: Error: ' + err);
+});
+db.on('open', function() {
+    console.log('Mongoose: Connection established');
+});
+db.on('disconnected', function() {
+    console.log('Mongoose: Connection stopped, recconect');
+    mongoose.connect(config.mongoURL, options);
+});
+db.on('reconnected', function () {
+    console.info('Mongoose reconnected!');
+});
+
 
 app.use(express.static(__dirname+'/Client'));
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    // we're connected!
-});
-
-mongoose.connect('mongodb://localhost:27017/paleta_db');
-
-var book = new Catalog({id:1, title:'אביויו', URL_cover:'', description:'ספר פעוטות'});
-book.save(function (err, book) {
-    if(err) return console.error(err);
-    console.dir(book);
-});
-
 app.get('/',function (req, res) {
       res.sendFile(path.join(__dirname+'/Client/index.html'));
 });
