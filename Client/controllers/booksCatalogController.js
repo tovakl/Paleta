8ff5ca51406921338da20c837e1ba5df8d11861f
Buzz2000 <bazibox@gmail.com>
2016-08-21 18:50:27 +0300

    Fix loading problem

--- Client/controllers/booksCatalogController.js
+++ Client/controllers/booksCatalogController.js
@@ -2,16 +2,38 @@ function booksCatalogController($scope, $http){
 
     $scope.books = {};
     $scope.currentId=0;
+    $scope.title={};
+    $scope.session = {};
     $scope.onLoad=onLoad;
     $scope.addToCart = addToCart;
     onLoad();
 
     function onLoad(){
-        $http.get('/booksCatalog/loadBooks')
+        var text;
+        var signed;
+        console.log("in on load");
+
+        $http.post('/userManage/getSessionInfo')
             .success(function(data){
-                $scope.books = data;
-                console.log("Succeed loading");
-        })
+                signed = data.signed;
+                $scope.session = data.session;
+
+                if(signed)
+                {
+                    document.getElementById("name").textContent = $scope.session.user.userName+" | ";
+
+                }
+
+                $http.get('/booksCatalog/loadBooks')
+                    .success(function(data){
+                        $scope.books = data;
+                        console.log("Succeed loading");
+                    })
+                    .error(function(data){
+                        console.log("Error: "+data);
+                    });
+
+            })
             .error(function(data){
                 console.log("Error: "+data);
             });
