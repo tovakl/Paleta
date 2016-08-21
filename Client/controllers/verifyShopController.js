1e44d9c438ecc20cc9557369cddfffc3ed9d9d16
Buzz2000 <bazibox@gmail.com>
2016-08-21 19:16:13 +0300

    Fix loading problem

--- Client/controllers/verifyShopController.js
+++ Client/controllers/verifyShopController.js
@@ -1,3 +1,34 @@
-/**
- * Created by Tamar mamo on 23/06/2016.
- */
+function verifyShopController($scope, $routeParams, $http) {
+
+
+    $scope.onLoad=onLoad;
+    $scope.title={};
+    $scope.session = {};
+
+
+    onLoad();
+
+    function onLoad(){
+        var text;
+        var signed;
+        console.log("in on load");
+
+        $http.post('/userManage/getSessionInfo')
+            .success(function(data){
+                signed = data.signed;
+                $scope.session = data.session;
+
+                if(signed)
+                {
+                    document.getElementById("name").textContent = $scope.session.user.userName+" | ";
+
+                }
+
+            })
+            .error(function(data){
+                console.log("Error: "+data);
+            });
+    }
+
+}
+
