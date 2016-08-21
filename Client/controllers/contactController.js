48773c467b3ca55d1a92a34e488bf26bfd4e90fb
Buzz2000 <bazibox@gmail.com>
2016-08-21 19:11:32 +0300

    Fix loading problem

--- Client/controllers/contactController.js
+++ Client/controllers/contactController.js
@@ -1,4 +1,31 @@
 function contactController($scope,$routeParams, $http)
 {
+    $scope.onLoad=onLoad;
+    $scope.title={};
+    $scope.session = {};
 
+
+    onLoad();
+
+    function onLoad(){
+        var text;
+        var signed;
+        console.log("in on load");
+
+        $http.post('/userManage/getSessionInfo')
+            .success(function(data){
+                signed = data.signed;
+                $scope.session = data.session;
+
+                if(signed)
+                {
+                    document.getElementById("name").textContent = $scope.session.user.userName+" | ";
+
+                }
+
+            })
+            .error(function(data){
+                console.log("Error: "+data);
+            });
+    }
 }
