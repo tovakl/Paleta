8e9c578761d1df94f298bd23f7525975f7f028a2
Buzz2000 <bazibox@gmail.com>
2016-08-21 19:13:14 +0300

    Fix loading problem

---
+++ Client/controllers/paintsCatalogController.js
@@ -0,0 +1,34 @@
+function paintCatalogController($scope, $routeParams, $http) {
+
+
+    $scope.onLoad=onLoad;
+    $scope.title={};
+    $scope.session = {};
+
+
+    onLoad();
+
+    function onLoad(){
+        var text;
+        var signed;
+        console.log("in on load");
+
+        $http.post('/userManage/getSessionInfo')
+            .success(function(data){
+                signed = data.signed;
+                $scope.session = data.session;
+
+                if(signed)
+                {
+                    document.getElementById("name").textContent = $scope.session.user.userName+" | ";
+
+                }
+
+            })
+            .error(function(data){
+                console.log("Error: "+data);
+            });
+    }
+
+}
+
