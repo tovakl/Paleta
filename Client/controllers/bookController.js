d3e4f7a49a69811300ba9f5b92519d68dfb434fa
Buzz2000 <bazibox@gmail.com>
2016-08-21 18:53:26 +0300

    Fix loading problem

--- Client/controllers/bookController.js
+++ Client/controllers/bookController.js
@@ -1,6 +1,8 @@
 function bookController($scope, $routeParams, $http)
 {
     $scope.onLoad = onLoad;
+    $scope.title={};
+    $scope.session = {};
     $scope.bookInfo = {};
     $scope.addToCart = addToCart;
 
@@ -9,18 +11,37 @@ function bookController($scope, $routeParams, $http)
     console.log("in the client side controller");
     onLoad();
 
-    function onLoad()
-    {
-        $http.get('/booksInfo/loadBookInfo/'+bookId)
-            .success(function (data) {
-                $scope.bookInfo = data;
+    function onLoad(){
+        var text;
+        var signed;
+        console.log("in on load");
+
+        $http.post('/userManage/getSessionInfo')
+            .success(function(data){
+                signed = data.signed;
+                $scope.session = data.session;
+
+                if(signed)
+                {
+                    document.getElementById("name").textContent = $scope.session.user.userName+" | ";
+
+                }
+
+                $http.get('/booksInfo/loadBookInfo/'+bookId)
+                    .success(function (data) {
+                        $scope.bookInfo = data;
+                    })
+                    .error(function (data) {
+                        console.log("Error: "+data);
+
+                    })
+
             })
-            .error(function (data) {
+            .error(function(data){
                 console.log("Error: "+data);
-
-            })
+            });
     }
-
+    
     function addToCart(id)
     {
         console.log("in add to cart "+ id);
